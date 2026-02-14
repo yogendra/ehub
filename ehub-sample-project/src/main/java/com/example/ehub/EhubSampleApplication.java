a5e2ce193a2e9df3b36f008e3234775e5dc0d8ac
Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>
2026-02-14 21:08:17 +0800

    Sample project
    Signed-off-by: Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>

---
+++ ehub-sample-project/src/main/java/com/example/ehub/EhubSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhubSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhubSampleApplication.class, args);
+	}
+
+}
